8fbcb681c3314a128818a644eef0ce9a20059e28
Kevin Sua <auxiliartic3@maxicassa.com>
2024-09-16 12:33:24 -0500

    FRONTEND Y BACKEND 0.1

--- server/src/db/connection.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-
-const sequelize = new Sequelize('almacen', 'root', 'Tamaguchi13', {
-    host: 'localhost',
-    dialect: 'mysql'
-  });
-
-  export default sequelize;
--- server/src/index.ts
+++
@@ -1,6 +0,0 @@
-import Server from "./models/server";
-import dotenv from 'dotenv'
-
-//Configuramos las variables de entorno
-dotenv.config();
-const server = new Server;
