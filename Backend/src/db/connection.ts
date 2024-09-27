1df00888b97d9d53d140ce3f8ec1723f3c2dd616
Kevin Sua <auxiliartic3@maxicassa.com>
2024-09-27 16:18:56 -0500

    cambios hasta agregar producto funcional

---
+++ Backend/src/db/connection.ts
@@ -0,0 +1,11 @@
+import { Sequelize } from "sequelize";
+
+
+const db = new Sequelize('simulador', 'postgres', 'Tamaguchi13', {
+    host: 'localhost',
+    dialect: 'postgres'
+  });
+
+  
+
+  export default db;
